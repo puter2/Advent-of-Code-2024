c78246c6dc9c325f8a2244989fd8d07c53799d37
Piotr Zając <piotrzajac4@gmail.com>
2024-12-07 14:35:39 +0100

    day 6 done

--- day 6/guard.py
+++ day 6/guard.py
@@ -26,3 +26,60 @@ class Guard:
                 dir_pnt += 1
                 dir_pnt %= 4
         return len(seen)
+    
+    def makeCycle(self):
+        obstacles = self.room.getObstacles()
+        pos = self.room.getStart()
+        borders = self.room.getDimensions()
+        directions = [
+            (-1,0),
+            (0,1),
+            (1,0),
+            (0,-1),
+        ]
+        dir_pnt = 0
+        seen = set()
+        ans = set()
+        while -1 < pos[0] < borders[0] and -1 < pos[1] < borders[1]:
+            dx, dy = directions[dir_pnt]
+            next_pos = (pos[0] + dx, pos[1] + dy)
+            if next_pos not in obstacles and next_pos not in seen:
+                #print(f'test isCycle({dx,dy,pos,next_pos})')
+                if self.isCycle(dir_pnt,pos,next_pos):
+                    ans.add(next_pos)
+            if next_pos not in obstacles:
+                #print(pos)
+                seen.add(pos)
+                pos = next_pos
+            else:
+                dir_pnt += 1
+                dir_pnt %= 4
+        ans.discard(self.room.getStart())
+        print(ans)
+        return len(ans)
+
+    def isCycle(self, direction, start_pos, extra_obst):
+        obstacles = self.room.getObstacles()
+        obstacles.add(extra_obst)
+        pos = start_pos
+        borders = self.room.getDimensions()
+        directions = [
+            (-1,0),
+            (0,1),
+            (1,0),
+            (0,-1),
+        ]
+        dir_pnt = direction
+        seen = set()
+        while -1 < pos[0] < borders[0] and -1 < pos[1] < borders[1]:
+            dx, dy = directions[dir_pnt]
+            next_pos = (pos[0] + dx, pos[1] + dy)
+            if (next_pos, dir_pnt) in seen or (pos, dir_pnt) in seen:
+                return True
+            if next_pos not in obstacles:
+                seen.add((pos, dir_pnt))
+                pos = next_pos
+            else:
+                dir_pnt += 1
+                dir_pnt %= 4
+        return False
--- day 6/main.py
+++ day 6/main.py
@@ -3,11 +3,12 @@ from guard import Guard
 
 r = Room('day 6\\input.txt')
 
-print(r.grid)
-print(r.getStart())
-print(r.getObstacles())
-print(r.getDimensions())
+# print(r.grid)
+# print(r.getStart())
+# print(r.getObstacles())
+# print(r.getDimensions())
 
 g = Guard(r)
 
 print(g.traverse())
+print(g.makeCycle())
