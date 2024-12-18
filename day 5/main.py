01bf6fd32c06c2090aa193f03690749ad349eb44
piotr <piotrzajac4@gmail.com>
2024-12-18 11:10:56 +0100

    day5 done

--- day 5/main.py
+++ day 5/main.py
@@ -7,10 +7,16 @@ print(r.getOrder())
 print(r.getSamples())
 
 res = 0
+res2 = 0
 for sample in r.getSamples():
     if Solver().isValid(r.getOrder(), sample):
         res += Solver().getMiddle(sample)
+    else:
+        fixed = Solver().fixSample(r.getOrder(), sample)
+        # print(Solver().fixSample(r.getOrder(), sample))
+        res2 += Solver().getMiddle(fixed)
     # print(sample)
     # print(Solver().isValid(r.getOrder(), sample))
 
 print(res)
+print(res2)
--- day 5/solver.py
+++ day 5/solver.py
@@ -12,3 +12,19 @@ class Solver:
 
     def getMiddle(self, sample):
         return int(sample[len(sample) // 2])
+    
+    def fixSample(self, order, sample):
+        fixed = [sample[0]]
+        for page in sample[1:]:
+            if order.get(page) == None:
+                fixed.append(page)
+            else:
+                found = False
+                for i in range(len(fixed)):
+                    if fixed[i] in order.get(page):
+                        fixed.insert(i, page)
+                        found = True
+                        break
+                if not found:
+                    fixed.append(page)
+        return fixed
